5c80bb8f165c3b5ef32477f3528eb1ffcc4c1075
gouqichun <525458513@qq.com>
2025-05-17 23:55:44 +0800

    feat(client): 添加客户端功能以处理服务器响应和用户交互
    新增DealResponse方法用于处理服务器响应并输出到标准输出
    添加菜单功能以支持公聊、私聊、更新用户名和退出操作
    实现Run方法以启动客户端业务逻辑

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -11,6 +13,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +21,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 	//链接server
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d",serverIp, serverPort))
@@ -27,6 +31,121 @@ func NewClient(serverIp string, serverPort int) *Client {
 	}
 	client.conn = conn
 	return client
+}
+
+//处理server回应的消息，直接显示到标准输出即可
+func (client *Client) DealResponse() {
+	//一旦client.conn有数据，就直接copy到stdout标准输出上，永久阻塞监听
+	io.Copy(os.Stdout, client.conn)
+}
+
+func (client *Client) menu() bool{
+	var flag int
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+	fmt.Scanln(&flag)
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	}else{
+		fmt.Println(">>>>>请输入合法范围内的数字...")
+		return false
+	}
+}
+
+func (client *Client) PublicChat() {
+	//提示用户输入消息
+	var chatMsg string
+	fmt.Println(">>>>>请输入聊天内容，exit退出")
+	fmt.Scanln(&chatMsg)
+	for chatMsg != "exit" {
+		//消息不为空则发送给服务器
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err!= nil {
+				fmt.Println("conn.Write error:", err)
+				break
+			}
+		}
+		chatMsg = ""
+		fmt.Println(">>>>>请输入聊天内容，exit退出")
+		fmt.Scanln(&chatMsg)
+	}
+}
+
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err!= nil {
+		fmt.Println("conn.Write error:", err)
+		return
+	}
+}
+
+func (client *Client) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+	client.SelectUsers()
+	fmt.Println(">>>>>请输入聊天对象[用户名],exit退出")
+	fmt.Scanln(&remoteName)
+	for remoteName!= "exit" {
+		fmt.Println(">>>>>请输入聊天内容，exit退出")
+		fmt.Scanln(&chatMsg)
+		for chatMsg!= "exit" {
+			_, err := client.conn.Write([]byte("to|" + remoteName + "|" + chatMsg + "\n"))
+			if err!= nil {
+				fmt.Println("conn.Write error:", err)
+				break
+			}
+			chatMsg = ""
+			fmt.Println(">>>>>请输入聊天内容，exit退出")
+			fmt.Scanln(&chatMsg)
+		}
+		client.SelectUsers()
+		fmt.Println(">>>>>请输入聊天对象[用户名],exit退出")
+		fmt.Scanln(&remoteName)
+	}
+}
+func (client *Client) UpdateName() bool {
+	fmt.Println(">>>>>请输入用户名:")
+	fmt.Scanln(&client.Name)
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write error:", err)
+		return false
+	}
+	return true
+}
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+		}
+		switch client.flag {
+			case 1:
+				//公聊模式
+				client.PublicChat()
+				fmt.Println("公聊模式选择成功...")
+				break
+			case 2:
+				//私聊模式
+				client.PrivateChat()
+				fmt.Println("私聊模式选择成功...")
+				break
+			case 3:
+				//更新用户名
+				fmt.Println("更新用户名选择成功...")
+				client.UpdateName()
+				break
+			case 0:
+				fmt.Println("退出...")
+				break
+				//退出
+		}
+	}
 }
 
 var serverIp string
@@ -45,8 +164,10 @@ func main() {
 		fmt.Println(">>>>>链接服务器失败...")
 		return
 	}
+	//单独开启一个goroutine去处理server的回执消息
+	go client.DealResponse()
 	fmt.Println(">>>>>链接服务器成功...")
 
 	//启动客户端业务
-	for{}
+	client.Run()
 }
